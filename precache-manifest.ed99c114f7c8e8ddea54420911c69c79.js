58d625ed1db9f1c47d2c7f6775ee8945c4d22613
Phil <nherwin@Nherwins-MacBook-Pro.local>
2020-01-15 12:25:34 +0800

    Updates

---
+++ precache-manifest.ed99c114f7c8e8ddea54420911c69c79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3cfd31f8611a240b9414f299503d30",
+    "url": "/pixiloop/index.html"
+  },
+  {
+    "revision": "d29a50b8008dc956b7c8",
+    "url": "/pixiloop/static/css/main.3b22b524.chunk.css"
+  },
+  {
+    "revision": "d32989d2c2031eaceaa8",
+    "url": "/pixiloop/static/js/2.28791510.chunk.js"
+  },
+  {
+    "revision": "d29a50b8008dc956b7c8",
+    "url": "/pixiloop/static/js/main.2ecbbf25.chunk.js"
+  },
+  {
+    "revision": "1ce3e19e2a40a2175f79",
+    "url": "/pixiloop/static/js/runtime-main.ffc75c6c.js"
+  }
+]);
